85a87c0d1db89fbcfd6f312cfc31abd4dc4feeba
IsaacSec <isaacsecundino3@gmail.com>
2017-08-22 20:12:10 +0000

    MainActivity layout completed

--- app/src/main/java/com/example/isaac/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/isaac/weatherapp/MainActivity.java
@@ -2,16 +2,40 @@ package com.example.isaac.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Spinner locationSpinner = (Spinner) findViewById(R.id.locationSpinner);
-    private String[] locations = {"Mexico  City", "Paris", "Madrid", "Caracas", ""};
+    private Spinner locationSpinner;
+    private String[] locations = {"Mexico  City", "Paris", "Madrid", "Caracas", "London"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViewObjects();
+        setupLocationSpinner();
+    }
+
+    private void initViewObjects () {
+        locationSpinner = (Spinner) findViewById(R.id.locationSpinner);
+    }
+
+    private void setupLocationSpinner () {
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, locations);
+
+        locationSpinner.setAdapter(spinnerAdapter);
+
+        locationSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
     }
 }
